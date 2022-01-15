8e33f0ef03b38158ca76802902a870e0796ace6d
alisdemir33 <alisdemir33@gmail.com>
2022-01-15 05:20:28 +0300

    loading lazy with spinner not optimized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { Route, Redirect,Routes, Navigate } from 'react-router-dom';
+import React,{Suspense} from 'react';
+import classes from './components/layout/Layout.module.css';
 
-import AllQuotes from './pages/AllQuotes';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import NotFound from './pages/NotFound';
 import Layout from './components/layout/Layout';
 import Comments  from './components/comments/Comments';
+/* import AllQuotes from './pages/AllQuotes'; 
+import NewQuote from './pages/NewQuote';*/
+
+const NewQuote =React.lazy( () => {return  import('./pages/NewQuote')});
+const AllQuotes =React.lazy( () => {return  import('./pages/AllQuotes')});
 
 function App() {
+  const loadingDiv= (
+    <div className={classes.loading}>
+       <LoadingSpinner message={"TESSSSSSSSSSSSSSSSSTTTTTT"}/>                        
+    </div>
+  ) ;
+
   return (
     <Layout>
+       <Suspense fallback = {loadingDiv}> 
       <Routes>
         <Route path='/' element={ <Navigate to ='/quotes'/>} />    
         <Route path='/quotes' element={<AllQuotes />}/>
@@ -20,6 +33,7 @@ function App() {
         <Route path='/new-quote' element={ <NewQuote />} />
         <Route path='*' element={ <NotFound />}/>
       </Routes>
+       </Suspense> 
     </Layout>
   );
 }
